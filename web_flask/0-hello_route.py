fd4e0bc49216d3ec0bb166e218620cba4f109e8d
KiprotichKibor <kiborotich24@gmail.com>
2024-05-22 05:09:35 -0700

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ Starts a flask web application
 """
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """ returns Hello HBNB! """
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -5,15 +5,18 @@ Starts a flask web application
 """
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """ returns Hello HBNB! """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ returns HBNB """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
@@ -21,12 +24,14 @@ def c_text(text):
     text_no_underscore = text.replace('_', ' ')
     return "C {}".format(text_no_underscore)
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def py_text(text='is cool'):
     """ display “Python ”, followed by the value of the text variable """
     text_no_underscore = text.replace('_', ' ')
     return "Python {}".format(text_no_underscore)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
